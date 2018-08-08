88e743fbcb72a62badae46be9119cdb6693ea820
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-08-08 21:36:51 +0100

    reduce

--- js_tutorial/functional.js
+++ js_tutorial/functional.js
@@ -6,7 +6,7 @@ function urlify(string) {
 
 function imperativeUrls(elements) {
     let urls = [];
-    elements.forEach(function(element) {
+    elements.forEach(function (element) {
         urls.push(urlify(element));
     });
     return urls;
@@ -27,7 +27,7 @@ console.log(functionalUrls(states));
 
 function imperativeSingles(elements) {
     let singles = [];
-    elements.forEach(function(element) {
+    elements.forEach(function (element) {
         if (element.split(/\s+/).length === 1) {
             singles.push(element);
         }
@@ -45,3 +45,47 @@ function functionalSingles(elements) {
 
 console.log(functionalSingles(states));
 
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function imperativeSum(elements) {
+    let total = 0;
+    elements.forEach(function (n) {
+        total += n;
+    });
+    return total;
+}
+
+console.log(imperativeSum(numbers));
+
+// numbers.reduce((total, n) => { return total += n; }, 0);
+
+function functionalSum(elements) {
+    return elements.reduce((total, n) => {
+        return total += n;
+    }, 0);
+}
+
+console.log(functionalSum(numbers));
+
+// lengths: Imperative solution
+
+function imperativeLengths(elements) {
+    let lengths = {};
+    elements.forEach(function (element) {
+        lengths[element] = element.length;
+    });
+    return lengths;
+}
+
+console.log(imperativeLengths(states));
+
+// functional 
+
+function functionalLengths(elements) {
+    return elements.reduce((lengths, element) => {
+        lengths[element] = element.length;
+        return lengths;
+    }, {});
+}
+
+console.log(functionalLengths(states));
